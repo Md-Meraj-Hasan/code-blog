977075ade5b4a8339721d2c049c872df935fa89f
Md-Meraj-Hasan <167162296+Md-Meraj-Hasan@users.noreply.github.com>
2026-05-28 21:23:05 +0300

    Create price categories for car analysis
    This script categorizes car prices into segments based on MSRP ranges, labeling them as Budget, Mid Range, Premium, Luxury, and Ultra Luxury for better analysis.

---
+++ static/projects/Cars Data Analysis/analysis_4.py
@@ -0,0 +1,18 @@
+"""
+Create price categories based on MSRP ranges
+and assign labels like Budget, Premium,and
+Luxury to better analyze car price segments
+"""
+
+msrp_bins = [0, 20000, 40000, 70000, 120000, 1000000]
+msrp_labels =[
+  'Budget', 'Mid Range', 'Premium',
+  'Luxury', 'Ultra Luxury'
+]
+
+df['Price_Group'] = pd.cut(
+  df['MSRP'], bins=msrp_bins, labels=msrp_labels
+)
+
+# Checking minimum and maximum MSRP value 
+df["MSRP"].agg(['min', 'max'])
