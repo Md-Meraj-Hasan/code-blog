b206e5bc958450a23ef560dd176bc1af189a4ba4
Md-Meraj-Hasan <167162296+Md-Meraj-Hasan@users.noreply.github.com>
2026-05-28 18:02:54 +0300

    Implement data loading function for CSV
    Added a main function to load data from a CSV file.

--- static/projects/Cars Data Analysis/analysis_1.py
+++ static/projects/Cars Data Analysis/analysis_1.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
+# Main Function to Data Load
 def load_data(database):
     df = pd.read_csv(database)
+    return df
     
 data = "Cars_data.csv"
 load_data(data)
 
 df.head(20)
+
+# Display First 20 rows of Dataset 
